30bf862e1172475dcb72de2afb2a55b1717e5739
HexaField <10372036+HexaField@users.noreply.github.com>
2025-12-28 12:41:41 +1100

    Add husky pre-commit hook and update lint-staged configuration; format code style in index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,2 @@
-export const SHARED_CONSTANT = "ConnectionEngine";
-export const add = (a: number, b: number) => a + b;
+export const SHARED_CONSTANT = 'ConnectionEngine'
+export const add = (a: number, b: number) => a + b
